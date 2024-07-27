52177723ad9174d011b5ec7dd0ac8e109ef63382
noirzxc0 <noirxxx000@gmail.com>
2024-07-27 12:57:48 +0800

    commit

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,13 @@ connectDB();
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://music-archive-seven.vercel.app/"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 
 // Routes
 app.get("/", (req, res) => {
